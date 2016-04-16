8eed958d0e9b6321d82da0109fda16311041ecad
Zain Adil <zainadil@outlook.com>
2016-04-16 15:29:04 -0400

    Testing some stuff

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
+  console.log(req);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
