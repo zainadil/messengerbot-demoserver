0ec8e5f4f7579b391b44a5db3c5fa824bb98d402
Zain Adil <zainadil@outlook.com>
2016-04-16 15:24:33 -0400

    Added Post Request for Webhooks

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 })
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(text);
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.listen(process.env.PORT, function () {
   console.log('Example app listening');
 });
