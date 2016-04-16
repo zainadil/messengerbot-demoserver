2d6dedc78c15db3f03cafd3f7505866c3f9d0bcb
Zain Adil <zainadil@outlook.com>
2016-04-16 15:55:39 -0400

    Adding requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser  = require('body-parser');
+var request = require('request');
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
