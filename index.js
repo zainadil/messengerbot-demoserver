5abd82c9a54dd256d7e6f0fb98b1b8d74938af39
Zain Adil <zainadil@outlook.com>
2016-04-16 15:17:00 -0400

    Adding Webhook route

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(process.env.PORT, function () {
   console.log('Example app listening');
 });
